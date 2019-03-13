a833f8c70e881bd6d0dfc4c2a5b49d51ad1fb618
Guillaume Champagne <guillaume.champagne@polymtl.ca>
2019-03-13 17:05:40 -0400

    cygproc

--- FreeRTOS/Demo/CORTEX_A7_BCM2836/Drivers/printf.h
+++ FreeRTOS/Demo/CORTEX_A7_BCM2836/Drivers/printf.h
@@ -1,6 +1,16 @@
 #ifndef PRINTF_H_INCLUDED
 #define PRINTF_H_INCLUDED
 
+#include <stdint.h>
+
+typedef struct {
+	uint32_t ev_id;
+	void* this_fn;
+	void* call_site;
+	uint32_t lowtimestamp;
+	uint32_t hightimestamp;
+} cyg_event_t;
+
 void printf(const char *fmt, ...);
 void sprintf(char *buffer, const char *fmt, ...);
 
